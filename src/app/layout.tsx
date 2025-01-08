a084554333f01682d053d56e6d6e2e7593720790
N4thanJ <nathan.jordens@student.ucll.be>
2025-01-08 15:11:04 +0100

    Added footer credits

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,8 +24,12 @@ export default function RootLayout({
           </header>
           <main className="flex-grow container my-8">{children}</main>
           <footer className="bg-secondary py-4">
-            <div className="container text-center text-secondary-foreground">
-              © 2025 Prisma Schema Generator
+            <div className="container text-center text-secondary-foreground space-x-8">
+              <span>© 2025 Prisma Schema Generator</span>
+              <a href="https://github.com/N4thanJ" target="_blank">
+                Created By{" "}
+                <span className="text-blue-800 font-bold">N4thanJ</span>
+              </a>
             </div>
           </footer>
         </div>
